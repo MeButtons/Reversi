4b69d019535aa2c3459a0b623f9ee242be05d9c8
Shewpdanewb <Gijs.nieuwkoop@gmail.com>
2016-10-14 12:38:49 +0200

    tadaaaa

--- Reversi/Program.cs
+++ Reversi/Program.cs
@@ -9,17 +9,16 @@ namespace Reversi
 {
     class Veld : Form
     {
-      
-        
 
        public Veld()
         {
-            Button[,] veld = new Button[5, 5];
-            for (int i = 0; i < 5; i++)
+            int n = 4;
+            Button[,] veld = new Button[n, n];
+            for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < n; j++)
                 {
-
+                    veld[i, j] = new Button();
                     veld[i,j].Size = new Size(50,50);
                     veld[i,j].Location = new Point(i * 50, j * 50);
 
