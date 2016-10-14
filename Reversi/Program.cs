5503b465b540d6951d20472cceec48629b49ff0c
Mike Knoop <mike_knoop_mail@yahoo.com>
2016-10-14 12:23:45 +0200

    ?

--- Reversi/Program.cs
+++ Reversi/Program.cs
@@ -3,9 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Reversi
 {
+    class Veld : Form
+    {
+      
+        
+
+       public Veld()
+        {
+            Button[,] veld = new Button[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    veld[i,j].Size = new Size(50,50);
+                    veld[i,j].Location = new Point(i * 50, j * 50);
+
+                    this.Controls.Add(veld[i,j]);
+                }
+
+            }
+        }
+    }
+
+
+    
+
+
+
+
     static class Program
     {
         /// <summary>
@@ -14,9 +44,9 @@ namespace Reversi
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Veld bord;
+            bord = new Veld();
+            Application.Run(bord);
         }
     }
 }
