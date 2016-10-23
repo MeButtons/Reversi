cec78b2b16d4c9d7c78b3053f23f2738a68826b0
Mike Knoop <mike_knoop_mail@yahoo.com>
2016-10-23 17:03:17 +0200

    stenen teller toegevoegd

--- Reversi/Program.cs
+++ Reversi/Program.cs
@@ -9,13 +9,17 @@ namespace Reversi
 {
     class Veld : Form
     {
-        int beurt = 1;
-       
+        int beurt = 1, aantalblauw=0, aantalrood=0;
+        int lengte = 400, breedte = 400;
+        private Label LabelBlauw, LabelRood;
         public Veld()
         {
+            this.Size = new Size(lengte,breedte);
             int n = 4;
             Button[,] veld = new Button[n, n];
             bool[,] gespeeld = new bool[n, n];
+           
+
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
@@ -23,7 +27,7 @@ namespace Reversi
                     veld[i, j] = new Button();
                     veld[i, j].BackColor = Color.LightGray;
                     veld[i, j].Size = new Size(50, 50);
-                    veld[i, j].Location = new Point(i * 50, j * 50);
+                    veld[i, j].Location = new Point(i * 50 + 50, j * 50 + 50);
 
                     this.Controls.Add(veld[i, j]);
                     veld[i, j].Click += veldkleur;
@@ -36,7 +40,19 @@ namespace Reversi
             settings.Size = new Size(80, 50);
             settings.Location = new Point((n + 1) * 50);
             settings.Text = "Settings";
+
+            this.LabelBlauw = new Label();
+            this.LabelBlauw.Location = new Point(25, 12);
+            LabelBlauw.Text = "Blauw:" + aantalblauw;
+
+            this.LabelRood = new Label();
+            this.LabelRood.Location = new Point(200, 12);
+            LabelRood.Text = "Rood:" + aantalrood;
+
+
             this.Controls.Add(settings);
+            this.Controls.Add(LabelBlauw);
+            this.Controls.Add(LabelRood);
  
         }
 
@@ -46,18 +62,22 @@ namespace Reversi
             
             Button myButton = sender as Button;
 
-            if (myButton.BackColor != Color.Red && myButton.BackColor != Color.Blue)
+            if (myButton.BackColor != Color.Red && myButton.BackColor != Color.Blue)            // controle of de steen al een keer gespeeld is
             {
                 if (beurt == 1)
                 {
                     myButton.BackColor = Color.Red;
-
+                    aantalrood = aantalrood + 1;
                     beurt = 2;
+                    LabelRood.Text = "Rood:" + aantalrood;
                 }
                 else if (beurt == 2)
                 {
                     myButton.BackColor = Color.Blue;
+                    aantalblauw = aantalblauw + 1;
                     beurt = 1;
+                    LabelBlauw.Text = "Blauw:" + aantalblauw;
+                    
                 }
             }
         }
